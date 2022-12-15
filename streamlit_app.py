68ea186adde849368d653e3e2d94af57c282b064
rajjona <120607347+rajjona@users.noreply.github.com>
2022-12-15 16:23:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('New Healthy Diner')
 
-streamlit.header('🍌🍍Build your own smoothie🥝🍇')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toast')
+streamlit.header('🍌🍍Build your own Fruit smoothie🥝🍇')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
                
